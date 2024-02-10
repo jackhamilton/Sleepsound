60c642cffc012f545ab507aedfbfbd11c9aaa625
Jack Hamilton <jack@vifive.ai>
2024-02-10 04:36:50 -0600

    UI Tests and Presets

---
+++ Snapshots/SnapshotsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SnapshotsLaunchTests.swift
+//  Snapshots
+//
+//  Created by Jack Hamilton on 2/10/24.
+//
+
+import XCTest
+
+final class SnapshotsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
